5bb89e940b8746119cf5e6cd2b149f1b352d39ae
Léon Lenclos <contact@leonlenclos.net>
2022-11-24 13:03:05 +0100

    Fix unescaped char bug in export

--- js/export.js
+++ js/export.js
@@ -21,6 +21,18 @@ function formatString (str, values) {
     return str;
 }
 
+function addSlashes(str){
+    return str.replace(/\\/g, '\\\\').
+        replace(/\u0008/g, '\\b').
+        replace(/\t/g, '\\t').
+        replace(/\n/g, '\\n').
+        replace(/\f/g, '\\f').
+        replace(/\r/g, '\\r').
+        replace(/'/g, '\\\'').
+        replace(/"/g, '\\"').
+        replace(/`/g, '\\`');
+}
+
 function exportStandAlone(data, title){
   const doc = getDOM('template.html')
   let sources = {}
@@ -41,7 +53,7 @@ function exportStandAlone(data, title){
   const serializer = new XMLSerializer();
   let output = serializer.serializeToString(doc)
   output = formatString(output, sources)
-  output = formatString(output, {title:title, gamedata:data})
+  output = formatString(output, {title:title, gamedata:addSlashes(data)})
   download(output, title, 'html');
   return doc
 }
